808ba8fd62a7bc62c64693008849d224771724cb
RaphaelBrugier <raphael.brugier@gmail.com>
2013-02-28 23:27:07 +0100

    fix javasource parsing of method with primitive returned types

--- src/test/org/langera/freud/optional/javasource/methoddecl/MethodDeclarationJdomTest.java
+++ src/test/org/langera/freud/optional/javasource/methoddecl/MethodDeclarationJdomTest.java
@@ -19,6 +19,7 @@
 
 package org.langera.freud.optional.javasource.methoddecl;
 
+import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.filter.ElementFilter;
 import org.junit.Before;
@@ -28,7 +29,9 @@ import org.langera.freud.optional.javasource.block.CodeBlock;
 import org.langera.freud.optional.javasource.parser.JavaSourceTokenType;
 
 import java.io.StringReader;
+import java.util.Iterator;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -45,13 +48,67 @@ public class MethodDeclarationJdomTest
                     "  {" +
                     "       return this.getClass();" +
                     "  }" +
+                    " " +
+                    "  public byte primitiveByte()" +
+                    "  {" +
+                    "       return 0;" +
+                    "  }" +
+                    " " +
+                    "  public short primitiveShort()" +
+                    "  {" +
+                    "       return 0;" +
+                    "  }" +
+                    " " +
+                    "  public int primitiveInt()" +
+                    "  {" +
+                    "       return 0;" +
+                    "  }" +
+                    " " +
+                    "  public long primitiveLong()" +
+                    "  {" +
+                    "       return 0;" +
+                    "  }" +
+                    " " +
+                    "  public float primitiveFloat()" +
+                    "  {" +
+                    "       return 0;" +
+                    "  }" +
+                    " " +
+                    "  public double primitiveDouble()" +
+                    "  {" +
+                    "       return 0;" +
+                    "  }" +
+                    " " +
+                    "  public char primitiveChar()" +
+                    "  {" +
+                    "       return 'a';" +
+                    "  }" +
+                    " " +
+                    "  public boolean primitiveBoolean()" +
+                    "  {" +
+                    "       return true;" +
+                    "  }" +
+                    "  public void voidMethod()" +
+                    "  {" +
+                    "       return true;" +
+                    "  }" +
+                    " " +
                     "}";
-    private Element methodDecl;
-
+    private Element methodDeclToString;
+    private Element methodDeclPrimitiveByte;
+    private Element methodDeclPrimitiveShort;
+    private Element methodDeclPrimitiveInt;
+    private Element methodDeclPrimitiveLong;
+    private Element methodDeclPrimitiveFloat;
+    private Element methodDeclPrimitiveDouble;
+    private Element methodDeclPrimitiveChar;
+    private Element methodDeclPrimitiveBoolean;
+    private Element methodDeclVoidMethod;
+    
     @Test
     public void shouldParseMethodImplementation() throws Exception
     {
-        MethodDeclarationJdom methodDeclarationJdom = new MethodDeclarationJdom(methodDecl, null);
+        MethodDeclarationJdom methodDeclarationJdom = new MethodDeclarationJdom(methodDeclToString, null);
 
         assertEquals("toString", methodDeclarationJdom.getName());
         assertEquals("String", methodDeclarationJdom.getReturnType());
@@ -60,14 +117,73 @@ public class MethodDeclarationJdomTest
         assertNotNull(impl);
         assertEquals(1, impl.getMethodCallListByMethodName("getClass").size());
     }
+    
+    @Test
+    public void shouldParseMethodImplementationWithPrimitiveReturnedTypes() throws Exception
+    {
+    	MethodDeclarationJdom primitiveByteMethodDeclaration = new MethodDeclarationJdom(methodDeclPrimitiveByte, null);
+    	MethodDeclarationJdom primitiveShortMethodDeclaration = new MethodDeclarationJdom(methodDeclPrimitiveShort, null);
+    	MethodDeclarationJdom primitiveIntMethodDeclaration = new MethodDeclarationJdom(methodDeclPrimitiveInt, null);
+    	MethodDeclarationJdom primitiveLongMethodDeclaration = new MethodDeclarationJdom(methodDeclPrimitiveLong, null);
+    	MethodDeclarationJdom primitiveFloatMethodDeclaration = new MethodDeclarationJdom(methodDeclPrimitiveFloat, null);
+    	MethodDeclarationJdom primitiveDoubleMethodDeclaration = new MethodDeclarationJdom(methodDeclPrimitiveDouble, null);
+    	MethodDeclarationJdom primitiveCharMethodDeclaration = new MethodDeclarationJdom(methodDeclPrimitiveChar, null);
+        MethodDeclarationJdom primitiveBooleanMethodDeclaration = new MethodDeclarationJdom(methodDeclPrimitiveBoolean, null);
+
+        assertEquals("primitiveByte", primitiveByteMethodDeclaration.getName());
+        assertEquals("byte", primitiveByteMethodDeclaration.getReturnType());
+        
+        assertEquals("primitiveShort", primitiveShortMethodDeclaration.getName());
+        assertEquals("short", primitiveShortMethodDeclaration.getReturnType());
+        
+        assertEquals("primitiveInt", primitiveIntMethodDeclaration.getName());
+        assertEquals("int", primitiveIntMethodDeclaration.getReturnType());
+        
+        assertEquals("primitiveLong", primitiveLongMethodDeclaration.getName());
+        assertEquals("long", primitiveLongMethodDeclaration.getReturnType());
+        
+        assertEquals("primitiveFloat", primitiveFloatMethodDeclaration.getName());
+        assertEquals("float", primitiveFloatMethodDeclaration.getReturnType());
+        
+        assertEquals("primitiveDouble", primitiveDoubleMethodDeclaration.getName());
+        assertEquals("double", primitiveDoubleMethodDeclaration.getReturnType());
+        
+        assertEquals("primitiveChar", primitiveCharMethodDeclaration.getName());
+        assertEquals("char", primitiveCharMethodDeclaration.getReturnType());
+        
+        assertEquals("primitiveBoolean", primitiveBooleanMethodDeclaration.getName());
+        assertEquals("boolean", primitiveBooleanMethodDeclaration.getReturnType());
+    }
+    
+    @Test
+    public void shouldParseMethodImplementationWithVoidReturn() throws Exception
+    {
+    	MethodDeclarationJdom voidMethodDeclaration = new MethodDeclarationJdom(methodDeclVoidMethod, null);
+        assertEquals("voidMethod", voidMethodDeclaration.getName());
+        assertEquals("void", voidMethodDeclaration.getReturnType());
+    }
 
     @Before
     public void setUp() throws Exception
     {
         JavaSourceJdom javaSourceJdom = new JavaSourceJdom(new StringReader(CLASS_EXAMPLE), "Name");
 
-        methodDecl = (Element) javaSourceJdom.getDocument().
-                getDescendants(new ElementFilter(JavaSourceTokenType.FUNCTION_METHOD_DECL.getName())).next();
+        Document document = javaSourceJdom.getDocument();
+        System.out.println(document);
+		Iterator methodsWithReturnedType = document.
+                getDescendants(new ElementFilter(JavaSourceTokenType.FUNCTION_METHOD_DECL.getName()));
+		methodDeclToString = (Element) methodsWithReturnedType.next();
+		methodDeclPrimitiveByte = (Element) methodsWithReturnedType.next();
+		methodDeclPrimitiveShort = (Element) methodsWithReturnedType.next();
+		methodDeclPrimitiveInt = (Element) methodsWithReturnedType.next();
+		methodDeclPrimitiveLong = (Element) methodsWithReturnedType.next();
+		methodDeclPrimitiveFloat = (Element) methodsWithReturnedType.next();
+		methodDeclPrimitiveDouble = (Element) methodsWithReturnedType.next();
+		methodDeclPrimitiveChar = (Element) methodsWithReturnedType.next();
+		methodDeclPrimitiveBoolean = (Element) methodsWithReturnedType.next();
+		
+		methodDeclVoidMethod=(Element)	document.
+        getDescendants(new ElementFilter(JavaSourceTokenType.VOID_METHOD_DECL.getName())).next();
     }
 
 }
