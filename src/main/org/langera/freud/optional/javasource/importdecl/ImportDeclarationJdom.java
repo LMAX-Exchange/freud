d3d99756c9ab752cd2e98667558a8754554c3476
Steve Newson <Steven.Newson@gmail.com>
2013-05-23 09:18:17 +0000

    Correct a typo in the import declaration interface.

--- src/main/org/langera/freud/optional/javasource/importdecl/ImportDeclarationJdom.java
+++ src/main/org/langera/freud/optional/javasource/importdecl/ImportDeclarationJdom.java
@@ -35,7 +35,7 @@ public final class ImportDeclarationJdom implements ImportDeclaration
     }
 
     @Override
-    public boolean isStaticDecalaration()
+    public boolean isStaticDeclaration()
     {
         return staticImport;
     }
